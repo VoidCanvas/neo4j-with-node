3be873e511201a8c00f26edbc672c2ffc641a782
metalshan <go2mailshan@gmail.com>
2016-02-13 15:33:55 +0530

    parameterized route

--- app.js
+++ app.js
@@ -4,38 +4,74 @@ var app = express();
 
 var txUrl = "http://localhost:7474/db/data/transaction/commit";
 
+//normal index page output
 app.get('/', function (req, res) {
   res.send('<h2>This is a demo of neo4j, a graph database</h2>');
 });
 
+
+// data get route returns the node we created
 app.get('/data', function (req, res) {
-	var query="MATCH (n) RETURN n LIMIT {limit}";
-	var params={limit: 100}
+	var query="MATCH (n:VoidCanvas) RETURN n LIMIT 100";
 
-	var promise = doDatabaseOperation(query,params)
+	var promise = doDatabaseOperation(query);
 	promise.then(function (data) {
 		res.send(data)
 	})
 
 });
 
+// data post route to create a node of type "VoidCanvas" and name attribute as "Paul"
 app.post('/data', function (req, res) {
-	var query="CREATE (n {name:{myParam}}) RETURN n";
-	var params={myParam: "Paul"}
+	var query="CREATE (n:VoidCanvas {name:'Paul'}) RETURN n";
 
-	var promise = doDatabaseOperation(query,params)
+	var promise = doDatabaseOperation(query)
 	promise.then(function (data) {
 		res.send(data)
 	})
 
 });
+
+
+// data get route returns the node we created
+app.get('/data/:name', function (req, res) {
+	var query="MATCH (n:VoidCanvas {name: {nameParam}}) RETURN n LIMIT 100";
+	var params = {
+		nameParam : req.params.name
+	}
+
+	var promise = doDatabaseOperation(query, params);
+	promise.then(function (data) {
+		res.send(data)
+	})
+
+});
+
+// data post route to create a node of type "VoidCanvas" and name attribute as "Paul"
+app.post('/data/:name', function (req, res) {
+	var query="CREATE (n:VoidCanvas {name:{nameParam}}) RETURN n";
+	
+	var params = {
+		nameParam : req.params.name
+	}
+
+	var promise = doDatabaseOperation(query, params);
+	promise.then(function (data) {
+		res.send(data)
+	})
+
+});
+
+
+
+
 
 var doDatabaseOperation = function (query, params) {
 	return new Promise(function (resolve, reject) {
 		request.post({
 			uri:txUrl,
 			headers:{
-				"Authorization": "Basic bmVvNG"
+				"Authorization": "Basic bmVvNGo6bmVvNGoy"
 			},
 			json:{
 				statements:[
